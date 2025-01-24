ca87c4afad87b73c6f3eec848ca02a09f773ff61
Mathis <90316879+Akkuun@users.noreply.github.com>
2025-01-24 12:02:31 +0100

    prefabe breakable

---
+++ Assets/Scripts/Obstacle/BreakableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    // Ici, tu peux ajouter des fonctionnalités pour ton objet cassable
+    // Par exemple, il pourrait se casser ou être détruit
+}
