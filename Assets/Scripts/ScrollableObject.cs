d7620423978f3169c5caff533925a42cda70791d
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-01-24 09:05:02 +0100

    Added Singleton SpeedManager for all terrain elements

--- Assets/Scripts/ScrollableObject.cs
+++ Assets/Scripts/ScrollableObject.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class ScrollableObject : MonoBehaviour
 {
-    public float speed = 1.0f;
+    private SpeedManager speedManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        speedManager = SpeedManager.instance;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
+        transform.position += Vector3.left * speedManager.GetSpeed() * Time.deltaTime;
         /*
         if (transform.position.x <= destroyX)
         {
---
+++ Assets/Scripts/SpeedManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpeedManager", menuName = "Scriptable Objects/SpeedManager")]
+public class SpeedManager : MonoBehaviour
+{
+    public static SpeedManager instance;
+    [SerializeField]
+    public float speed = 1.0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public float GetSpeed()
+    {
+        return speed;
+    }
+}
